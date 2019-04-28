f9cff007e0a2559e4cc4d14a3577e7f1eefb04f3
Your Name <you@example.com>
2019-04-28 11:16:19 +0800

    php float

--- 6.php_float.php
+++ 6.php_float.php
@@ -3,5 +3,18 @@ $f = 0.58;
 var_dump(intval($f * 100));//57
 var_dump(intval(bcmul($f , 100,2)));//58
 var_dump(normalizer_normalize($f * 101));//58
+
+
+if (empty(0)) {
+	echo "0 is empty\n";
+}else{
+	echo "0 is not empty\n";
+}
+
+if (empty(0.00)) {
+	echo "0.00 is empty\n";
+}else{
+	echo "0.00 is not empty\n";
+}
 ?>
 
